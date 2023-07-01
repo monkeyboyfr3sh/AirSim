7f790ffa84706e5c6d61b862d71885cac77d9266
David Cain <d.cain2740@gmail.com>
2023-07-01 06:56:44 -0500

    .

--- PythonClient/detection/nav_ui_impl.py
+++ PythonClient/detection/nav_ui_impl.py
@@ -9,42 +9,42 @@ import detection_utils as dt_util
 from lidar_plotter import LidarPlotter
 import threading
 import simulation_tasks as sim_tasks 
+import logging
 
-DRONE_HEIGHT = -10
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+DRONE_HEIGHT = -10  # Default height of the drone
 
 class AirSimConnectWorker(QtCore.QThread):
-    frameCaptured = QtCore.pyqtSignal(QtGui.QImage)
-    finished = QtCore.pyqtSignal(QtGui.QImage)
+    frameCaptured = QtCore.pyqtSignal(QtGui.QImage)  # Signal emitted when a frame is captured
+    finished = QtCore.pyqtSignal(QtGui.QImage)  # Signal emitted when the worker is finished
 
     def __init__(self, parent=None):
         super().__init__(parent)
-        print("airsim_connect thread init")
-        self.connect_command = True
+        logger.info("airsim_connect thread init")
+        self.connect_command = True  # Flag to control the connection
 
     def run(self):
-        print("airsim_connect thread entering")
+        logger.info("airsim_connect thread entering")
         # Connect to the AirSim simulator
-        print("airsim_connect thread connecting")
+        logger.info("airsim_connect thread connecting")
         client = airsim.MultirotorClient()
         client.confirmConnection()
 
-        # Turn on detection
+        # Turn on object detection for specific filters
         detect_filter = "Monument*"
         dt_util.detection_filter_on_off(client, True, detect_filter)
         detect_filter = "Car*"
         dt_util.detection_filter_on_off(client, True, detect_filter)
         detect_filter = "Deer*"
         dt_util.detection_filter_on_off(client, True, detect_filter)
-        # detect_filter = "Raccoon*"
-        # dt_util.detection_filter_on_off(client, True, detect_filter)
-        # detect_filter = "InstancedFoliageAct*"
-        # detection_filter_on_off(client, True, detect_filter)
 
         while self.connect_command:
-            # Decode raw image 
+            # Capture frame from the simulator
             png = dt_util.get_fpv_frame(client=client)
     
-            # Now run detect process
+            # Run object detection on the captured frame
             detect_objects = dt_util.get_detected_object(client)
             if detect_objects is not None:
                 for obj in detect_objects:
@@ -53,10 +53,10 @@ class AirSimConnectWorker(QtCore.QThread):
             else:
                 detect_list = []
 
-            # Draw HUD
+            # Draw HUD (Heads-Up Display) on the frame
             dt_util.draw_HUD(png, client)
 
-            # Push image into queue
+            # Convert and emit the captured frame as QImage
             try:
                 png = cv2.cvtColor(png, cv2.COLOR_BGRA2BGR)
                 h, w, ch = png.shape
@@ -64,14 +64,14 @@ class AirSimConnectWorker(QtCore.QThread):
                 image = QtGui.QImage(png.tobytes(), w, h, bytes_per_line, QtGui.QImage.Format.Format_BGR888)
                 self.frameCaptured.emit(image)
             except:
-                print("err")
+                logger.error("An error occurred")
                 pass
 
-        # Cleanup
-        print("airsim_connect thread cleaning up")
+        # Cleanup after the connection is stopped
+        logger.info("airsim_connect thread cleaning up")
         client.enableApiControl(False)
         client.armDisarm(False)
-        print("airsim_connect thread exiting")
+        logger.info("airsim_connect thread exiting")
         self.finished.emit(QtGui.QImage())  # Emit the 'finished' signal when the worker is done
 
 class NavUIDialog(QtWidgets.QDialog, nav_gui_base.Ui_Dialog):
@@ -79,42 +79,49 @@ class NavUIDialog(QtWidgets.QDialog, nav_gui_base.Ui_Dialog):
         super().__init__(parent)
         self.setupUi(self)
 
-        self.airsimConnectWorker = None
+        self.airsimConnectWorker = None  # Worker for connecting to AirSim
 
+        # Connect button signals to respective slots
         self.airsimConnect_button.clicked.connect(self.airsim_connect)
         self.airsimDisconnect_button.clicked.connect(self.airsim_disconnect)
-
         self.resetButton.clicked.connect(self.reset)
         self.navigate_button.clicked.connect(self.navigte)
         self.stop_navigate_button.clicked.connect(self.stop_navigate)
 
-        self.target_name = "Monument_01_176"
-        # self.target_name = "Car_35"
-        self.default_z = DRONE_HEIGHT
-        self.task_thread = threading.Thread()
+        self.target_name = "Monument_01_176"  # Default target name
+        self.default_z = DRONE_HEIGHT  # Default z-coordinate
+        self.task_thread = threading.Thread()  # Thread for running tasks
 
     def blank_graphics_view(self):
-
+        """
+        Clears the graphics view and sets a blank screen or image background.
+        """
         # Blank the screen
         self.graphicsView.setScene(QtWidgets.QGraphicsScene())
         self.graphicsView.scene().clear()
 
-        # Try setting an immage as background
+        # Try setting an image as background
         try:
-            # Then display an image
+            # Load and display an image
             png = cv2.imread("./disconnect_meme.jpg")
             h, w, ch = png.shape
             bytes_per_line = ch * w
             image = QtGui.QImage(png.tobytes(), w, h, bytes_per_line, QtGui.QImage.Format.Format_BGR888)
             self.update_stream(image)
         except:
-            print("Err setting image")
+            logger.error("Error setting image")
 
     def closeEvent(self, event):
+        """
+        Overrides the closeEvent method to handle the closing of the dialog.
+        """
         pass
 
     def airsim_connect(self):
-        # Detect if we have worker
+        """
+        Connects to AirSim by creating and starting the AirSimConnectWorker.
+        """
+        # Detect if we have a worker
         if not self.airsimConnectWorker:
             self.airsimConnectWorker = AirSimConnectWorker()
             self.airsimConnectWorker.frameCaptured.connect(self.update_stream)
@@ -123,28 +130,45 @@ class NavUIDialog(QtWidgets.QDialog, nav_gui_base.Ui_Dialog):
             self.airsimConnectWorker.start()
 
     def airsim_disconnect(self):
-        # Detect if we have worker
+        """
+        Disconnects from AirSim by setting the connect_command of the AirSimConnectWorker to False.
+        """
+        # Detect if we have a worker
         if self.airsimConnectWorker:
             self.airsimConnectWorker.connect_command = False
 
     def navigte(self):
-        if (self.task_thread.is_alive()):
-            print("Task is already running!")
+        """
+        Starts the navigation task by creating a task client and running the 'navigate_to_monument' task in a thread.
+        """
+        if self.task_thread.is_alive():
+            logger.info("Task is already running!")
             return
 
         self.task_thread, task_client = sim_tasks.create_task_client(target=sim_tasks.navigate_to_monument,
-                                                                     args=(self.default_z,),start_task=True)
+                                                                     args=(self.default_z,), start_task=True)
 
     def stop_navigate(self):
-        self.task_thread, task_client = sim_tasks.create_task_client(target=sim_tasks.client_disarm,start_task=True)
+        """
+        Stops the navigation task by creating a task client and running the 'client_disarm' task in a thread.
+        """
+        self.task_thread, task_client = sim_tasks.create_task_client(target=sim_tasks.client_disarm,
+                                                                     start_task=True)
         self.task_thread = threading.Thread()
 
     def reset(self):
-        self.task_thread, task_client = sim_tasks.create_task_client(target=sim_tasks.sim_reset,start_task=True)
+        """
+        Resets the simulation by creating a task client and running the 'sim_reset' task in a thread.
+        """
+        self.task_thread, task_client = sim_tasks.create_task_client(target=sim_tasks.sim_reset,
+                                                                     start_task=True)
         self.task_thread = threading.Thread()
 
     @QtCore.pyqtSlot(QtGui.QImage)
     def update_stream(self, image):
+        """
+        Updates the graphics view with the provided image.
+        """
         pixmap = QtGui.QPixmap.fromImage(image)
         pixmap = pixmap.scaled(self.graphicsView.size(), QtCore.Qt.AspectRatioMode.KeepAspectRatio)
 
@@ -154,6 +178,9 @@ class NavUIDialog(QtWidgets.QDialog, nav_gui_base.Ui_Dialog):
 
     @QtCore.pyqtSlot(QtGui.QImage)
     def worker_finished(self, image):
-        print(f"AirSimConnectWorker has finished")  # Do something to handle the worker finishing
+        """
+        Handles the finishing of the AirSimConnectWorker by resetting the graphics view and worker variable.
+        """
+        logger.info("AirSimConnectWorker has finished")  # Do something to handle the worker finishing
         self.blank_graphics_view()
         self.airsimConnectWorker = None  # Reset the worker variable
--- PythonClient/detection/ui_launcher.py
+++ PythonClient/detection/ui_launcher.py
@@ -3,22 +3,32 @@ import airsim
 
 import cv2
 from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QMdiSubWindow,QMdiArea,QTextEdit
+from PyQt6.QtWidgets import QMdiSubWindow, QMdiArea, QTextEdit
 
 import nav_ui_impl as nav_ui
 import sub_window_impl as sub_window_ui
 
-def create_dialog(CustomDialogImpl: QtWidgets.QDialog, dialog_name = None, pos = (100,100)):
+def create_dialog(CustomDialogImpl: QtWidgets.QDialog, dialog_name=None, pos=(100, 100)):
+    """
+    Creates and configures a custom dialog.
 
+    Args:
+        CustomDialogImpl (QtWidgets.QDialog): Custom dialog class.
+        dialog_name (str): Name of the dialog window. Default is None.
+        pos (tuple): Position of the dialog window. Default is (100, 100).
+
+    Returns:
+        QtWidgets.QDialog: Created custom dialog.
+    """
     customDialog = CustomDialogImpl()
     customDialog.setWindowTitle(dialog_name)
     customDialog.move(pos[0], pos[0])
-    customDialog.setWindowFlags(customDialog.windowFlags() | 
-                          QtCore.Qt.WindowType.WindowMinimizeButtonHint |
-                          QtCore.Qt.WindowType.WindowMaximizeButtonHint |
-                          QtCore.Qt.WindowType.WindowMinMaxButtonsHint )
-    
-    # Read the the size then set fixed window
+    customDialog.setWindowFlags(customDialog.windowFlags() |
+                                QtCore.Qt.WindowType.WindowMinimizeButtonHint |
+                                QtCore.Qt.WindowType.WindowMaximizeButtonHint |
+                                QtCore.Qt.WindowType.WindowMinMaxButtonsHint)
+
+    # Read the size and set fixed window size
     size = customDialog.size()
     customDialog.setFixedSize(size)
     return customDialog
@@ -31,12 +41,12 @@ if __name__ == "__main__":
     dialog_list = []
 
     # Create the main dialog
-    dialog_list.append( create_dialog(nav_ui.NavUIDialog,
-                           pos=(100, 100),dialog_name="Main Window"))
-    # # Create the sub dialog
-    # dialog_list.append( create_dialog(sub_window_ui.SubWindowDialog,
-    #                            pos=(300, 300),dialog_name="Subby Wubby"))
+    dialog_list.append(create_dialog(nav_ui.NavUIDialog,
+                                     pos=(100, 100), dialog_name="Main Window"))
 
+    # Create the sub dialog
+    # dialog_list.append(create_dialog(sub_window_ui.SubWindowDialog,
+    #                                pos=(300, 300), dialog_name="Subby Wubby"))
 
     for dialog in dialog_list:
         dialog.show()
