cfe138aeab3697a4ccee68891bffb56d512708de
David Cain <d.cain2740@gmail.com>
2023-07-01 00:35:14 -0500

    Update nav_ui_impl.py

--- PythonClient/detection/nav_ui_impl.py
+++ PythonClient/detection/nav_ui_impl.py
@@ -3,12 +3,14 @@ import airsim
 
 import nav_gui_base
 
+import numpy as np
 import cv2
 from PyQt6 import QtCore, QtGui, QtWidgets
 from PyQt6.QtWidgets import QMdiSubWindow,QMdiArea,QTextEdit
 
 import detection_utils as dt_util
 from lidar_plotter import LidarPlotter
+import time
 
 from queue import Queue
 
@@ -16,47 +18,9 @@ from simulation_tasks import viewer_task
 
 DRONE_HEIGHT = -10
 
-class VideoStreamWorker(QtCore.QThread):
+class AirSimConnectWorker(QtCore.QThread):
     frameCaptured = QtCore.pyqtSignal(QtGui.QImage)
 
-    def __init__(self, png_queue, parent=None):
-        super().__init__(parent)
-        self.cap = None
-        self.png_queue = png_queue
-
-    def run(self):
-        # Start the stream
-        self.start_stream()
-        # Now loop 
-        while True:
-
-            # Check if the queue has a new image
-            if( not (self.png_queue.empty()) ):
-                print("got")
-
-                # Get the picture from queue
-                frame = self.png_queue.get()
-
-                h, w, ch = frame.shape
-                bytes_per_line = ch * w
-                image = QtGui.QImage(frame.data, w, h, bytes_per_line, QtGui.QImage.Format.Format_RGB888)
-                self.frameCaptured.emit(image)
-
-            # if not self.cap.isOpened():
-            #     break
-
-    def stop_stream(self):
-        pass
-        # if self.cap:
-        #     self.cap.release()
-        #     self.cap = None
-
-    def start_stream(self):
-        pass
-        # if not self.cap:
-        #     self.cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-class AirSimConnectWorker(QtCore.QThread):
     def __init__(self, png_queue, parent=None):
         super().__init__(parent)
         print("airsim_connect thread init")
@@ -90,13 +54,18 @@ class AirSimConnectWorker(QtCore.QThread):
 
             # Push image into queue
             try:
-                self.png_queue.put_nowait(png)
+                # Assuming your 4-channel image is stored as a cv2 Mat object named 'image'
+                png= cv2.cvtColor(png, cv2.COLOR_BGRA2BGR)
+                h, w, ch = png.shape
+                bytes_per_line = ch * w
+                image = QtGui.QImage(png.tobytes(), w, h, bytes_per_line, QtGui.QImage.Format.Format_BGR888)
+                self.frameCaptured.emit(image)
             except:
+                print("err")
                 pass
 
         # Cleanup
         print("airsim_connect thread cleaning up")
-        # cv2.destroyAllWindows() 
         client.enableApiControl(False)
         client.armDisarm(False)
 
@@ -110,37 +79,12 @@ class NavUIDialog(QtWidgets.QDialog, nav_gui_base.Ui_Dialog):
         self.videoStreamWorker = None
         self.airsimConnectWorker = None
 
-        # Setup navigate command buttons
-        self.navigate_button.clicked.connect(self.start_stream)
-        self.stop_navigate_button.clicked.connect(self.stop_stream)
+        # # Setup navigate command buttons
+        # self.navigate_button.clicked.connect(self.start_stream)
+        # self.stop_navigate_button.clicked.connect(self.stop_stream)
 
         self.airsimConnect_button.clicked.connect(self.airsim_connect)
         self.airsimDisconnect_button.clicked.connect(self.airsim_disconnect)
-
-    def start_stream(self):
-
-        # Detect if we have worker
-        if (not self.videoStreamWorker) and (not self.png_queue):
-            # Create the worker
-            self.videoStreamWorker = VideoStreamWorker(self.png_queue)
-            self.videoStreamWorker.frameCaptured.connect(self.update_stream)
-
-            # Now start the worker
-            self.videoStreamWorker.start()
-
-    def stop_stream(self):
-
-        # Detect if we have worker
-        if self.videoStreamWorker:
-            # Stop the worker
-            self.videoStreamWorker.stop_stream()
-            self.videoStreamWorker.terminate()
-
-            self.blank_graphics_view()
-
-            # Clear the var
-            self.videoStreamWorker = None
-
 
     @QtCore.pyqtSlot(QtGui.QImage)
     def update_stream(self, image):
@@ -156,13 +100,14 @@ class NavUIDialog(QtWidgets.QDialog, nav_gui_base.Ui_Dialog):
         self.graphicsView.scene().clear()
 
     def closeEvent(self, event):
-        self.stop_stream()
+        pass
 
     def airsim_connect(self):
         # Detect if we have worker
         if not self.airsimConnectWorker:
             self.png_queue = Queue(maxsize=10) 
             self.airsimConnectWorker = AirSimConnectWorker(self.png_queue)
+            self.airsimConnectWorker.frameCaptured.connect(self.update_stream)
             self.airsimConnectWorker.connct_command = True
             self.airsimConnectWorker.start()
 
@@ -170,7 +115,7 @@ class NavUIDialog(QtWidgets.QDialog, nav_gui_base.Ui_Dialog):
         # Detect if we have worker
         if self.airsimConnectWorker:
             self.airsimConnectWorker.connct_command = False
-            self.airsimConnectWorker.wait()
+            self.blank_graphics_view()
 
             # Clear the var
             self.airsimConnectWorker = None
